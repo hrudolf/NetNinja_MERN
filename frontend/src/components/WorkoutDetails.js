13da26b6a81a9af6c6ca000cacc2cf3f7f4ac444
hrudolf <h.rudolf@gmail.com>
2023-01-25 08:29:15 +0100

    Added date formatter

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,3 +1,5 @@
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+
 const WorkoutDetails = ({ workout, workouts, setWorkouts }) => {
     const handleClick = async () => {
         const response = await fetch(`/api/workouts/${workout._id}`, {
@@ -15,8 +17,9 @@ const WorkoutDetails = ({ workout, workouts, setWorkouts }) => {
             <h4>{workout.title}</h4>
             <p><strong>Load (kg): </strong>{workout.load}</p>
             <p><strong>Reps: </strong>{workout.reps}</p>
+            <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
             <p>{workout.createdAt.slice(0, 10)}</p>
-            <span onClick={handleClick}>Delete</span>
+            <span onClick={handleClick}>🗑️</span>
         </div>
     );
 }
